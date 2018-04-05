a3087cb2d4e73109fa507e8aa519507ee325ff49
Tateishi Tadatoshi <ishio39@gmail.com>
2018-04-05 22:13:29 +0900

    make CTRL Tap to ESC, Hold to CTRL.

--- keyboards/dz60/keymaps/tadatoshi/keymap.c
+++ keyboards/dz60/keymaps/tadatoshi/keymap.c
@@ -37,6 +37,7 @@
 #define KC_MMCR MO(MACR)
 #define KC_MD60 MO(DZ60)
 #define KC_LNAV LT(NAVI, KC_SPC)
+#define KC_CESC CTL_T(KC_ESC)
 
 #define KC_LATB LALT(KC_TAB)
 #define KC_SATB S(LALT(KC_TAB))
@@ -67,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = KC_KEYMAP_T(
            SESC,  1  ,  2  ,  3  ,  4  ,  5  ,  6  ,  7  ,  8  ,  9  ,  0  , MINS, EQL , BSLS, GRV ,
            TAB ,  Q  ,  W  ,  E  ,  R  ,  T  ,  Y  ,  U  ,  I  ,  O  ,  P  , LBRC, RBRC, BSPC,
-           LCTL,  A  ,  S  ,  D  ,  F  ,  G  ,  H  ,  J  ,  K  ,  L  , SCLN, QUOT, ENT ,
+           CESC,  A  ,  S  ,  D  ,  F  ,  G  ,  H  ,  J  ,  K  ,  L  , SCLN, QUOT, ENT ,
            LSFT,  Z  ,  X  ,  C  ,  V  ,  B  ,  N  ,  M  , COMM, DOT , SLSH, RSFT, MHKB,
            LCTL, LGUI, LALT, BSPC, LNAV, SPC , RALT, MMCR, MD60, RCTL),
 
